7285982df22c7e57f9fa332d49e2774bb02117ac
Christina Takara <christina.takara@gmail.com>
2024-12-05 10:21:07 -0600

    added examples

--- models/learners.mjs
+++ models/learners.mjs
@@ -9,7 +9,7 @@ import mongoose from 'mongoose';
 //     year: Number,
 //     avg: Number,
 //     campus: String
-    
+
 // })
 
 const learnerSchema = new mongoose.Schema({
@@ -47,8 +47,48 @@ const learnerSchema = new mongoose.Schema({
         default: "Remote",
         required: true
     }
-    
+}, {
+    timestamps: true
 })
+
+// I can add in some indexes
+learnerSchema.index({ name: 1});
+learnerSchema.index({ year: 1});
+learnerSchema.index({ avg: 1});
+learnerSchema.index({ campus: 1 });
+
+// ==================== Here are some examples of Schema Methods ====================
+// this will give me all of the learners that have the same campus and the same year
+// this find has two fields, and there is no operator, so it's an implicit $and
+learnerSchema.methods.getPeers = function () {
+    // console.log(cb)
+    return mongoose.model("Learner").find( { campus: this.campus, year: this.year});
+}
+
+// 
+learnerSchema.statics.findPassing = function () {
+    return this.find({avg: { $gte: 70}});
+}
+
+learnerSchema.statics.findByCampus = function (campus) {
+    return this.find({ campus });
+}
+
+// ===================================================================================
+//              query helpers
+// ===================================================================================
+learnerSchema.query.byName = function (name) {
+    return this.where({ name: new RegExp(name, "i")});
+}
+
+
+// ===================================================================================
+//              virtuals
+// ===================================================================================
+learnerSchema.virtual('passing').get(function () {
+    return this.avg >= 70;
+})
+
 
 // Compile the schema into a model and export it.
 // Models are used much like classes to create instances of objects
--- routes/learners.mjs
+++ routes/learners.mjs
@@ -9,17 +9,20 @@ router.get('/seed', async (req, res) => {
             {
                 name: 'Frodo',
                 enrolled: true,
-                year: 2024
+                year: 2024,
+                avg: 70
             },
             {
                 name: 'Spongebob',
                 enrolled: true,
-                year: 2020
+                year: 2020,
+                avg: 90
             },
             {
                 name: 'Hermione',
                 enrolled: true,
-                year: 2023
+                year: 2023,
+                avg: 30
             }
         ])
         res.send('success').status(200);
@@ -39,12 +42,44 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.get('/passing', async (req, res) => {
+    try {
+        const result = await Learner.findPassing();
+        res.json(result).status(200);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+// another way to get passing
+router.get('/passing2', async (req, res) => {
+    try {
+        const foundLearners = await Learner.find({});
+        // console.log( objectArray.find(e => e.name === 'John') )
+        const passingLearners = foundLearners.filter(e => e.avg >= 70);
+        res.status(200).json(passingLearners);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
 // S - Show         GET         READ - returns a specific element
 // This is my show route
 router.get('/:id', async (req, res) => {
     try {
         const foundLearner = await Learner.findById(req.params.id);
+        console.log(await foundLearner.getPeers());
         res.status(200).json(foundLearner);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+router.get('/:id/peers', async (req, res) => {
+    try {
+        const foundLearner = await Learner.findById(req.params.id);
+        const peers =  await foundLearner.getPeers();
+        res.status(200).json(peers);
     } catch (err) {
         res.status(400).send(err);
     }
