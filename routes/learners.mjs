1968d7ff4e4abbf7e2cd40790e1479d9a0c7d397
Christina Takara <christina.takara@gmail.com>
2024-12-05 08:44:11 -0600

    added SHOW route

--- routes/learners.mjs
+++ routes/learners.mjs
@@ -28,11 +28,23 @@ router.get('/seed', async (req, res) => {
     }
 })
 
-
+// I - Index         GET         READ - returns all of the entries
+// This is my index route
 router.get('/', async(req, res) => {
     try {
         const foundLearners = await Learner.find({});
         res.status(200).json(foundLearners);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+// S - Show         GET         READ - returns a specific element
+// This is my show route
+router.get('/:id', async (req, res) => {
+    try {
+        const foundLearner = await Learner.findById(req.params.id);
+        res.status(200).json(foundLearner);
     } catch (err) {
         res.status(400).send(err);
     }
